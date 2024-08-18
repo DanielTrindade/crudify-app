36845aa6b2134db2f671cb7cab3a3d69f51e7921
DanielTrindade <danieloliveiratrindade@gmail.com>
2024-08-18 17:00:01 -0400

    fix: Corrigindo a integração da aplicação next com a minha api nest e adicionando shadcn-ui a minha app e refatorando a tela de login.

--- crudify-frontend-app/app/(auth)/login/page.tsx
+++ crudify-frontend-app/app/(auth)/login/page.tsx
@@ -1,92 +1,118 @@
 "use client"
-
 import { useState } from "react";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Alert, AlertDescription } from "@/components/ui/alert";
 
-type FormValues = {
-  email: string;
-  password: string;
-}
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Endereço de email inválido.",
+  }),
+  password: z.string().min(6, {
+    message: "A senha deve ter pelo menos 6 caracteres.",
+  }),
+});
 
 const Login = () => {
   const router = useRouter();
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<FormValues>();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
-  const onSubmit = async (data: FormValues) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post("http://localhost:3000/auth/login", data);
-      if (response.status === 201 && response?.data?.accessToken !== '') {
-        localStorage.setItem("accessToken", response.data.accessToken);
-        localStorage.setItem("userId", response.data.userId);
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: values.email,
+        password: values.password,
+      });
+      console.log(result);
+      if (result?.error) {
+        setError("Credenciais inválidas. Por favor, tente novamente.");
+      } else if (result?.ok) {
         router.push("/produtos");
-      } else {
-        setErrorMessage("Credenciais inválidas. Por favor, tente novamente.");
       }
     } catch (error) {
-      setErrorMessage(
-        "Ocorreu um erro durante o login. Por favor, tente novamente."
-      );
+      setError("Ocorreu um erro durante o login. Por favor, tente novamente.");
     }
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900">
-      <div className="max-w-md w-full p-6 bg-white dark:bg-gray-800 rounded-md shadow-md">
-        <h1 className="text-2xl font-semibold mb-4">Login</h1>
-        {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block font-medium">
-              E-mail
-            </label>
-            <input
-              type="text"
-              id="email"
-              className="w-full border-gray-300 dark:bg-gray-700 dark:text-white rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
-              {...register("email",{
-                required:{
-                  value: true,
-                  message: "Por favor, digite seu email."
-                }
-              })}
-            />
-            {errors.email && <p className="text-red-500 mt-1">Por favor, insira seu email.</p>}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block font-medium">
-              Senha
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full border-gray-300 dark:bg-gray-700 dark:text-white rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
-              {...register("password",{
-                required:{
-                  value: true,
-                  message: "Por favor, digite sua senha."
-                }
-              })}
-            />
-            {errors.password && <p className="text-red-500 mt-1">Por favor, insira sua senha.</p>}
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600"
-          >
-            Entrar
-          </button>
-        </form>
-        <p className="mt-4">
-          Não tem uma conta? <a href="/sign-up" className="text-blue-500 hover:underline">Cadastre-se</a>.
-        </p>
-      </div>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Faça login para acessar sua conta</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {error && (
+            <Alert variant="destructive" className="mb-4">
+              <AlertDescription>{error}</AlertDescription>
+            </Alert>
+          )}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="seu@email.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="******" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                {form.formState.isSubmitting ? "Entrando..." : "Entrar"}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          <p className="text-sm text-center w-full">
+            Não tem uma conta?{" "}
+            <a href="/sign-up" className="font-medium text-blue-600 hover:text-blue-500">
+              Cadastre-se
+            </a>
+          </p>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
